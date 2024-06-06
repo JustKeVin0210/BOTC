aa3e892cf94fe9b426b83e547b78c536ecc8f80c
JustKeVin0210 <liujiye0210@foxmail.com>
2024-06-07 00:05:01 +0800

    夜晚行动顺序添加黄昏，黎明，爪牙信息，恶魔信息

--- write_xlsx.py
+++ write_xlsx.py
@@ -1,6 +1,7 @@
 import os
 
 import json
+import math
 from openpyxl import Workbook, load_workbook
 from openpyxl.drawing.image import Image as ExcelImage
 from openpyxl.styles import PatternFill, Alignment, Side, Border, Font
@@ -13,13 +14,22 @@ def write_excel(json_file, image_dir, save_dir):
     side_style = None
     format_dict = {
         "title": {
-            "fill": PatternFill("solid", fgColor="000000"),
+            "fill": PatternFill("solid", fgColor="FF000000"),
             "alignment": Alignment(horizontal='center', vertical='center'),
             "border": Border(left=Side(style=side_style),
                              bottom=Side(style=side_style),
                              right=Side(style=side_style),
                              top=Side(style=side_style)),
             "font": Font(name='宋体', bold=True, color="FFFFFFFF"),
+        },
+        "time": {
+            "fill": PatternFill("solid", fgColor="FF00FF00"),
+            "alignment": Alignment(horizontal='center', vertical='center'),
+            "border": Border(left=Side(style=side_style),
+                             bottom=Side(style=side_style),
+                             right=Side(style=side_style),
+                             top=Side(style=side_style)),
+            "font": Font(name='宋体', italic=True, color="FF000000"),
         },
         "townsfolk": {
             "team_cn": "镇民",
@@ -93,6 +103,37 @@ def write_excel(json_file, image_dir, save_dir):
         script_roles = json.load(f)
     if script_roles[0].get("id") == "_meta":
         script_roles.pop(0)
+    script_times = [
+        {
+            "id": "duskTRANS",
+            "name": "黄昏",
+            "team": "time",
+            "firstNight": 1,
+            "otherNight": 1,
+        },
+        {
+            "id": "minionTRANS",
+            "name": "爪牙信息",
+            "team": "minion",
+            "firstNight": 2000,
+            "otherNight": 0,
+        },
+        {
+            "id": "demonTRANS",
+            "name": "恶魔信息",
+            "team": "demon",
+            "firstNight": 3000,
+            "otherNight": 0,
+        },
+        {
+            "id": "dawnTRANS",
+            "name": "黎明",
+            "team": "time",
+            "firstNight": 12600,
+            "otherNight": 15000,
+        }
+    ]
+    script_roles += script_times
     script_roles_fisrt_night = sorted([i for i in script_roles if i.get("firstNight")],
                                       key=lambda x: x.get("firstNight"))
     script_roles_other_night = sorted([i for i in script_roles if i.get("otherNight")],
@@ -110,27 +151,43 @@ def write_excel(json_file, image_dir, save_dir):
     for i in range(len(script_roles_fisrt_night) + 1):
         ws.row_dimensions[i + 1].height = 15
 
-    role_first_team_list, role_other_team_list = [], []
+    role_first_team_list, role_other_team_list, merge_list = [], [], []
     for i, (role_first, role_other) in enumerate(zip(script_roles_fisrt_night, script_roles_other_night)):
         if role_first:
-            role_first_team_list.append(role_first.get("team"))
-            role_first_infolist = [None, role_first.get("name"), format_dict.get(role_first.get("team")).get("team_cn"),
-                                   role_first.get("script"), role_first.get("firstNight")]
-            id_first = role_first.get("id")
-            image_first = ExcelImage(os.path.join(image_dir, id_first.replace("TRANS", "").capitalize() + ".png"))
+            if role_first.get("script"):
+                role_first_infolist = [None, role_first.get("name"),
+                                       format_dict.get(role_first.get("team")).get("team_cn"),
+                                       role_first.get("script"), role_first.get("firstNight")]
+                id_first = role_first.get("id")
+                image_first = ExcelImage(os.path.join(image_dir, id_first.replace("TRANS", "").capitalize() + ".png"))
+            else:
+                role_first_infolist = [None, role_first.get("name"), None, None,
+                                       math.ceil(role_first.get("firstNight") / 100) * 100]
+                merge_list.append((f"B{i + 2}", f"D{i + 2}"))
+                id_first = role_first.get("id")
+                image_first = ExcelImage(os.path.join(image_dir, id_first.replace("TRANS", "").upper() + ".png"))
             image_first.width, image_first.height = 19, 19
             ws.add_image(image_first, f"{chr(65)}{i + 2}")
+            role_first_team_list.append(role_first.get("team"))
         else:
             role_first_team_list.append(None)
             role_first_infolist = [None] * data_len
         if role_other:
-            role_other_team_list.append(role_other.get("team"))
-            role_other_infolist = [None, role_other.get("name"), format_dict.get(role_other.get("team")).get("team_cn"),
-                                   role_other.get("script"), role_other.get("otherNight")]
-            id_other = role_other.get("id")
-            image_other = ExcelImage(os.path.join(image_dir, id_other.replace("TRANS", "").capitalize() + ".png"))
+            if role_other.get("script"):
+                role_other_infolist = [None, role_other.get("name"),
+                                       format_dict.get(role_other.get("team")).get("team_cn"),
+                                       role_other.get("script"), role_other.get("otherNight")]
+                id_other = role_other.get("id")
+                image_other = ExcelImage(os.path.join(image_dir, id_other.replace("TRANS", "").capitalize() + ".png"))
+            else:
+                role_other_infolist = [None, role_other.get("name"), None, None,
+                                       math.ceil(role_other.get("otherNight") / 100) * 100]
+                merge_list.append((f"G{i + 2}", f"I{i + 2}"))
+                id_other = role_other.get("id")
+                image_other = ExcelImage(os.path.join(image_dir, id_other.replace("TRANS", "").upper() + ".png"))
             image_other.width, image_other.height = 19, 19
             ws.add_image(image_other, f"{chr(65 + data_len)}{i + 2}")
+            role_other_team_list.append(role_other.get("team"))
         else:
             role_other_team_list.append(None)
             role_other_infolist = [None] * data_len
@@ -159,6 +216,11 @@ def write_excel(json_file, image_dir, save_dir):
                 cell.alignment = format_dict.get(role_other_team).get("alignment")
                 cell.border = format_dict.get(role_other_team).get("border")
                 cell.font = format_dict.get(role_other_team).get("font")
+
+    # 合并单元格
+    for merge_position in merge_list:
+        ws.merge_cells(range_string=f"{merge_position[0]}:{merge_position[1]}")
+
     # 修改打印样式
     ws.print_options.horizontalCentered = True
     ws.print_options.verticalCentered = True
